0efd7fab36257ee9689267b08beb3e17683ede9b
Berat Arif Gönül <gonulberat1@gmail.com>
2025-11-26 10:22:32 +0300

    deneme

--- admin/products.php
+++ admin/products.php
@@ -17,6 +17,7 @@ if (!$giris_yapildi) {
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet" />
   <link rel="stylesheet" href="css/style.css" />
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 
 <body>
